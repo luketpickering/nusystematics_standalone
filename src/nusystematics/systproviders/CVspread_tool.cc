eb237b1b18fc8e18852f0330d759c50b0b861978
Luke Pickering <luketpickering@gmail.com>
2025-05-21 20:27:32 +0000

    updates CVspead to take multiple alternate models, with locations configured from fcl

--- src/nusystematics/systproviders/CVspread_tool.cc
+++ src/nusystematics/systproviders/CVspread_tool.cc
@@ -4,7 +4,7 @@
 
 #include "RwFramework/GSyst.h"
 
- //GENIE-MC/Generator
+// GENIE-MC/Generator
 #include "Framework/GHEP/GHepParticle.h"
 #include "Framework/GHEP/GHepRecord.h"
 
@@ -13,247 +13,288 @@
 using namespace nusyst;
 using namespace systtools;
 
-// constructor passes up configuration object to base class for generic tool
-// initialization and initialises our local copies of paramIds to unconfigured
-//  flag values
 CVspread::CVspread(fhicl::ParameterSet const &params)
-    : IGENIESystProvider_tool(params) { //get values from associated fcl file
+    : IGENIESystProvider_tool(params) {}
 
-      //below are defined in .hh file
-      pidx_Params.push_back(kParamUnhandled<size_t>);
-      dial_infos.emplace_back(DialInfo{"CVspread"});
-      CVs.push_back(0);
-      Variations.emplace_back();
-} //close param
-
-
-//Setting up the loading of histogram
-void CVspread::LoadObject() {
-    // Open the reference ROOT file
-   RefROOT  = TFile::Open("reference3D.root", "READ");
-    if (!RefROOT || RefROOT->IsZombie()) {
-        std::cerr << "Error: Could not open -.root file which stores 'mapping object' reference." << std::endl;
-        return;
-    }
-
-    // Retrieve the histogram
-    ObjRef = RefROOT->Get<TH3D>("MapReference");
-    if (!ObjRef) {
-        std::cerr << "Error: Could not find 'mapping object' AKA histogram stored as a TObject." << std::endl;
-    }
+std::vector<std::unique_ptr<TH3D>> geths(std::string const &loc) {
+  TFile fin(loc.c_str(), "READ");
+  std::vector<std::unique_ptr<TH3D>> hists;
+  for (auto hn :
+       {"CC0pi", "CC1pip", "CC1pim", "CC1pi0", "CC2cpi", "CCNpi", "CCOther",
+        "NC0pi", "NC1pip", "NC1pim", "NC1pi0", "NC2cpi", "NCNpi", "NCOther"}) {
+    hists.emplace_back(fin.Get<TH3D>(hn));
+    hists.back()->SetDirectory(nullptr);
+  }
+  return hists;
 }
 
+void CVspread::LoadInputs() {
+  auto inputs = tool_options.get<fhicl::ParameterSet>("inputs");
 
-//Reads the fcl file - AKA, user deciding which parameter dials, and tweak values, and this reads it
-SystMetaData BuildSystMetaData(fhicl::ParameterSet const &ps,
-                                            paramId_t firstId) {
+  ref_xs = geths(inputs.get<std::string>("reference"));
+
+  for (auto &di : dial_infos) {
+    di.alt_xs = geths(inputs.get<std::string>(di.prettyname));
+  }
+}
+
+SystMetaData CVspread::BuildSystMetaData(fhicl::ParameterSet const &ps,
+                                         paramId_t firstId) {
   SystMetaData smd;
   SystParamHeader dial_variation_template;
+  dial_variation_template.isSplineable = true;
+  dial_variation_template.paramVariations = {0, 1};
 
-  // loop through the header struct
-  for (auto const &prettyname : dial_infos) {
-    SystParamHeader phdr = dial_variation_template; //short-hand for simplicity
-    phdr.prettyName = prettyname; //keep things neat - fcl name for tweak
+  fhicl::ParameterSet inputs;
+  inputs.put("ref", ps.get<std::string>("reference"));
 
-    // Set up parameter definition with a standard tool configuration form using helper function.
+  std::vector<std::string> alt_models;
+
+  for (auto const &altm :
+       ps.get<std::vector<fhicl::ParameterSet>>("alt_models")) {
+
+    SystParamHeader phdr = dial_variation_template;
+    phdr.prettyName = altm.get<std::string>("name");
+    alt_models.push_back(phdr.prettyName);
+
+    inputs.put(phdr.prettyName, altm.get<std::string>("input"));
+
     phdr.systParamId = firstId++;
 
-    // set any parameter-specific ParamHeader   metadata here
-    phdr.isSplineable = true;
-    phdr.paramVariations = {0,1};
-
-    // add it to the metadata list to pass back.
     smd.push_back(phdr);
   }
 
-  //Does not like being defined in .hh
-  fhicl::ParameterSet tool_options;
-
-  // Put any options that you want to propagate to the ParamHeaders options
+  tool_options.put("inputs", inputs);
+  tool_options.put("alt_models", alt_models);
   tool_options.put("verbosity_level", ps.get<int>("verbosity_level", 0));
 
   return smd;
 }
 
-//Sets up the response from the fcl values; if set up correctly, reference histogram is loaded
 bool CVspread::SetupResponseCalculator(
     fhicl::ParameterSet const &tool_options) {
   verbosity_level = tool_options.get<int>("verbosity_level", 0);
 
+  std::vector<std::string> alt_models =
+      tool_options.get<std::vector<std::string>>("alt_models");
+
   // grab the pre-parsed param headers object
   SystMetaData const &md = GetSystMetaData();
 
-//Does not like being defined in the .hh
-    std::vector<DialInfo> dial_infos;
+  for (auto altm : alt_models) {
 
-  for (size_t i = 0; i < dial_infos.size(); ++i) {
-
-    auto const &prettyname = dial_infos[i].prettyname;
-
-    if (!HasParam(md, prettyname)) {
+    if (!HasParam(md, altm)) {
       if (verbosity_level > 1) {
-        std::cout << "[INFO]: Don't have parameter " << prettyname
+        std::cout << "[INFO]: Don't have parameter " << altm
                   << " in SystMetaData. Skipping configuration." << std::endl;
       }
       continue;
     }
-    pidx_Params[i] = GetParamIndex(md, prettyname);
+
+    auto pid = GetParamIndex(md, altm);
 
     if (verbosity_level > 1) {
-      std::cout << "[INFO]: Have parameter " << prettyname
-                << " in SystMetaData with ParamId: " << pidx_Params[i]
-                << ". Configuring." << std::endl;
+      std::cout << "[INFO]: Have parameter " << altm
+                << " in SystMetaData with ParamId: " << pid << ". Configuring."
+                << std::endl;
     }
-
-    auto phdr = md[pidx_Params[i]]; // param_md
-
-    CVs[i] = phdr.centralParamValue; // the initial central value of parameter
-    Variations[i] = phdr.paramVariations; // the variation of parameter
-
-
-    //Temporary throw loop until fcl integer step resolved - clarification needed
-    //currently set to return only two weights - one for tweak = 0 [GENIE], and = 1 [NEUT]
-    if (Variations[i].size() > 2){
-      std::cout << "Only one tweak value may be given at this time." << std::endl; 
-      throw; 
-    }
-
-    // in the concrete version of this example we're going to configure a GENIE GReWeightNuXSecCCQE instance for each configured variation.
-    for (auto v : Variations[i]) {  // loop through the variations of each param e.g (-3,-2...,2,3)std in fcl file
-      
-        if (v < 0 || v > 1) {
-    //this will check to make sure param variations are within range
-          std::cout << "Tweak out of range - dial range is between 0 and 1." << std::endl;
-    throw;
+    dial_infos.push_back(DialInfo{altm, pid, {}});
   }
-    }
-  }
-  LoadObject();
+  LoadInputs();
   // returning cleanly
   return true;
 }
 
-//Produces a weight value based on the fcl values, and returns the weight (as "resp")
-event_unit_response_t CVspread::GetEventResponse(genie::EventRecord const &ev){
+namespace {
+struct Topology {
+  enum topo {
+    kCC0pi = 0,
+    kCC1pip,
+    kCC1pim,
+    kCC1pi0,
+    kCC2cpi,
+    kCCNpi,
+    kCCOther,
+    kNC0pi,
+    kNC1pip,
+    kNC1pim,
+    kNC1pi0,
+    kNC2cpi,
+    kNCNpi,
+    kNCOther,
+    kInvalid
+  };
+};
+} // namespace
+
+Topology::topo get_reweight_topology(genie::EventRecord const &ev) {
+
+  int nnu = 0;
+  int nclep = 0;
+  int nnuc = 0;
+  int npip = 0;
+  int npim = 0;
+  int npi0 = 0;
+  int ngamma = 0;
+  int nother = 0;
+
+  for (auto const &po : ev) {
+    genie::GHepParticle const &p =
+        dynamic_cast<genie::GHepParticle const &>(*po);
+
+    if (p.Status() != genie::kIStStableFinalState) {
+      continue;
+    }
+    switch (p.Pdg()) {
+    case -12:
+    case -14:
+    case -16:
+    case 12:
+    case 14:
+    case 16: {
+      nnu++;
+      break;
+    }
+    case -11:
+    case -13:
+    case -15:
+    case 11:
+    case 13:
+    case 15: {
+      nclep++;
+      break;
+    }
+    case 2212:
+    case 2112: {
+      nnuc++;
+      break;
+    }
+    case -211: {
+      npim++;
+      break;
+    }
+    case 211: {
+      npip++;
+      break;
+    }
+    case 111: {
+      npi0++;
+      break;
+    }
+    case 22: {
+      ngamma++;
+      break;
+    }
+    default: {
+      if (p.Pdg() < 1000000000) {
+        nother++;
+      }
+      break;
+    }
+    }
+  }
+
+  if (nclep > 1) {
+    return Topology::kCCOther;
+  }
+
+  if (nnu > 1) {
+    return Topology::kInvalid;
+  }
+
+  bool iscc = !((nnu == 1) && (nclep == 0));
+
+  if (nother) {
+    return iscc ? Topology::kCCOther : Topology::kNCOther;
+  }
+
+  if (ngamma) {
+    return iscc ? Topology::kCCOther : Topology::kNCOther;
+  }
+
+  if (npi0) {
+    return (npi0 > 1) ? (iscc ? Topology::kCCNpi : Topology::kNCNpi)
+                      : (iscc ? Topology::kCC1pi0 : Topology::kNC1pi0);
+  }
+
+  if ((npip + npim)) {
+    if ((npip + npim) > 2) {
+      return iscc ? Topology::kCCNpi : Topology::kNCNpi;
+    }
+    if ((npip + npim) == 2) {
+      return iscc ? Topology::kCC2cpi : Topology::kNC2cpi;
+    }
+    return (npip == 1) ? (iscc ? Topology::kCC1pip : Topology::kNC1pip)
+                       : (iscc ? Topology::kCC1pim : Topology::kNC1pim);
+  }
+
+  return iscc ? Topology::kCC0pi : Topology::kNC0pi;
+}
+
+event_unit_response_t CVspread::GetEventResponse(genie::EventRecord const &ev) {
 
   event_unit_response_t resp;
 
   SystMetaData const &md = GetSystMetaData();
 
+  genie::GHepParticle *ISLep = ev.Probe();
 
-  //Below explained in GHepRecord.cxx
-  //Returns the intial state probe particle i.e. the muon neutrino
-  genie::GHepParticle *ISLep = ev.Probe(); //Probe() constructed in GHepRecord.h for GHepParticle.h
-  
-  //Returns the final state primary lepton
-  genie::GHepParticle *FSLep = ev.FinalStatePrimaryLepton(); //FSPL() constructed in GHepRecord.h for GHepParticle.h
+  genie::GHepParticle *FSLep = ev.FinalStatePrimaryLepton();
 
-  genie::GHepParticle *TargetNucleon = ev.TargetNucleus(); //TargetNucleus() constructed in GHepRecord.h for GHepParticle.h
+  if (!FSLep || !ISLep) {
+    return resp;
+  }
 
-  /*  if (!FSLep || !ISLep) {
-    throw incorrectly_generated()
-        << "[ERROR]: Failed to find IS and FS lepton in event: "
-        << ev.Summary()->AsString();
-  }*/
+  auto topo = get_reweight_topology(ev);
 
-  //P4() is a TLorentzVector constructed in GHepParticle.h; stores Enu_true, px, py and pz components for particle
-  ISLepP4 = *ISLep->P4(); //Points to initial state four-vector
-  FSLepP4 = *FSLep->P4(); //Points to final state four-vector
-  emTransfer = (ISLepP4 - FSLepP4); //Calculates the energy-momentum transfer
+  if (topo == Topology::kInvalid) {
+    return resp;
+  }
 
-  //Initial state energy AKA true neutrino energy
-  Enu_true = ISLepP4.E();
-  //Enu_true = nu->fP.E(); / 1E3; //nuisance definition
-  //Enu_true = ISLep->P4.E();
+  auto em_transfer_GeV = (*ISLep->P4() - *FSLep->P4());
+  auto Q2_GeV = -em_transfer_GeV.Mag2();
+  auto q_0_GeV = em_transfer_GeV.E();
+  auto Enu_true = ISLep->P4()->E();
 
-  //Calculates the Q2 (the negatove of energy-momentum transfer squared)
-  Q2 = -emTransfer.Mag2(); //Mag2() just takes the square of the magntidue
+  constexpr static double m_p_GeV = 0.93827203;
 
-  q0 = emTransfer.E();
+  auto W_nuc_rest_GeV =
+      std::sqrt((-Q2_GeV) + (2 * m_p_GeV * q_0_GeV) + (m_p_GeV * m_p_GeV));
 
-  //Nuisance defines this as just the mass of the proton
-  //rather than extracting the ID of the target nucleon, returning the mass accordingly
+  // get individual axis bins and check that the value is in range.
+  int binX = ref_xs[topo]->GetXaxis()->FindBin(Enu_true);
+  if ((binX == 0) || (binX == ref_xs[topo]->GetXaxis()->GetNbins() + 1)) {
+    return resp;
+  }
+  int binY = ref_xs[topo]->GetYaxis()->FindBin(W_nuc_rest_GeV);
+  if ((binY == 0) || (binY == ref_xs[topo]->GetYaxis()->GetNbins() + 1)) {
+    return resp;
+  }
+  int binZ = ref_xs[topo]->GetZaxis()->FindBin(Q2_GeV);
+  if ((binZ == 0) || (binZ == ref_xs[topo]->GetZaxis()->GetNbins() + 1)) {
+    return resp;
+  }
 
-  //Will extract the nucleon mass from event so the process exists; but will use proton mass until Nuisnace is changed
-  //m_nucleon = *TargetNucleon->Mass();
+  int binGlobal = ref_xs[topo]->GetBin(binX, binY, binZ);
 
-  //W_nuc_rest = sqrt((-Q2)+(2*m_nucleon*q0)+(m_nucleon*m_nucleon)); //W_nuc_rest calculated for target nucleon
-
-  W_nuc_rest = sqrt((-Q2)+(2*m_proton*q0)+(m_proton*m_proton)); //W_nuc_rest matching nuisance
-
-
-
-  //Determine bin from calculated W_nuc_rest (x), Q2 (y), and Enu_true (z)
-  int binX = ObjRef->GetXaxis()->FindBin(W_nuc_rest);
-  int binY = ObjRef->GetYaxis()->FindBin(Q2);
-  int binZ = ObjRef->GetZaxis()->FindBin(Enu_true);
-
-  int binREF = ObjRef->GetBin(binX, binY, binZ);
-            
   // Get the bin content
-  double binContent = ObjRef->GetBinContent(binREF);
-
-
-//Determine bin weight based on tweaks
+  double ev_ref_xs = ref_xs[topo]->GetBinContent(binGlobal);
 
   // loop through and calculate weights
-  for (size_t i = 0; i < dial_infos.size(); ++i) {
-    // this parameter wasn't configured, nothing to do
-    if (pidx_Params[i] == kParamUnhandled<size_t>) {
-      continue;
-    }
+  for (auto const &di : dial_infos) {
 
-    auto param_id = md[pidx_Params[i]].systParamId;
-    // initialize the response array with this paramId
-    resp.push_back({param_id, {}});
+    resp.push_back({di.pid, {}});
 
-    // loop through variations for this parameter
-    for (auto tweak : Variations[i]) {
+    double ev_xs_ratio = di.alt_xs[topo]->GetBinContent(binGlobal) / ev_ref_xs;
 
-      // put the response weight for this variation of this parameter into the
-      // response object
-      double REFweight = 1 + tweak*(binContent-1);
-      resp.back().responses.push_back(REFweight);
-      if (verbosity_level > 3) {
-        std::cout << "[DEBG]: For parameter " << md[pidx_Params[i]].prettyName
-                  << " at variation[" << tweak << "] = " << Variations[i][tweak]
-                  << " calculated weight: " << resp.back().responses.back()
-                  << std::endl;
-      }
+    resp.back().responses = {1.0, 1.0 + (ev_xs_ratio - 1.0)};
+    if (verbosity_level > 3) {
+      std::cout << "[DEBG]: For parameter " << di.prettyname << " at variation["
+                << 1 << "] = " << 1
+                << " calculated weight: " << resp.back().responses.back()
+                << std::endl;
     }
   }
 
-  return resp; //returns the final calculated, tweak adjusted, weight(s)
+  return resp;
 }
-
-//Nuisance Reference Info
-//MCStudies/GenericFlux_Vectors.cxx useful reference
-
-  //Returns the invariant hadronic mass
-  //W = ev.Summary()->Kine().W(true);
-  //Summary() constructed in GENIE~Framework/GHEP/GHepRecord.h
-  //Kine() constructed in GENIE~Framework/Interaction/Interaction.h
-  //W(bool) constructed in GENIE~Framework/Interaction/Kinematics.h
-
-  //Invariant Mass assuming nucleon is at rest
-  //Defined in nuisance, not nusystematics - must calculate internally
-      // Get W_true with assumption of initial state nucleon at rest
-
-//nuisance calculation
-/*    
-    
-    float m_n = (float)PhysConst::mass_proton;
-    // Q2 assuming nucleon at rest
-    W_nuc_rest = sqrt(-Q2 + 2 * m_n * q0 + m_n * m_n);
-    W = W_nuc_rest; // For want of a better thing to do
-    // True Q2
-    x = Q2 / (2 * m_n * q0);
-    y = 1 - ELep / Enu_true;*/
-
-  /*nuisance definitions
-  m_n = extracted from PhysConst namespace defined in nuisance/src/Utils/PhysConst.h - only proton mass
-  q0 = (nu->fP - lep->fP).E() / 1E3;
-  Q2 = -1 * (nu->fP - lep->fP).Mag2() / 1E6;
-
-  1Ex is a preset factor for nuisance for scaling the event histogram - not relevant for nusystematics
-  */
--- src/nusystematics/systproviders/CVspread_tool.hh
+++ src/nusystematics/systproviders/CVspread_tool.hh
@@ -3,10 +3,9 @@
 #include "nusystematics/interface/IGENIESystProvider_tool.hh"
 
 #include "TFile.h"
-#include "TTree.h"
 #include "TH3D.h"
 #include "TLorentzVector.h"
-
+#include "TTree.h"
 
 #include <array>
 #include <memory>
@@ -27,7 +26,7 @@ public:
   //'Second' configuration step: parameter headers
   // - Reads the preconstructed SystMetaData produced by BuildSystMetaData
   //   to configure an instance of this class to calculate weights
-  // - Recieves a copy of the tool_options instance constructed by 
+  // - Recieves a copy of the tool_options instance constructed by
   //   BuildSystMetaData as an argument
   bool SetupResponseCalculator(fhicl::ParameterSet const &);
 
@@ -42,38 +41,25 @@ public:
   // representing this instance as a string.
   std::string AsString() { return "CVspread"; }
 
-  ~CVspread(){}
+  ~CVspread() {}
 
 private:
   // arbitrary additional configuration from the tool configuration/parameter
   // headers can be storeds here
   fhicl::ParameterSet tool_options;
 
-  // The ParamHeaders id of the free parameters provided by this systprovider
-  std::vector<size_t> pidx_Params;
+  void LoadInputs();
 
-  // The configured variations to precalculate for the parameters
-  std::vector<double> CVs;
-  std::vector<std::vector<double>> Variations;
- 
-  void LoadObject();
+  std::vector<std::unique_ptr<TH3D>> ref_xs;
 
-  TFile *RefROOT; //TFile for the reference root file containing our 3D object
-  TH3D *ObjRef; //Name of our 3D object (TH3D histogram)
-
-  TLorentzVector ISLepP4, FSLepP4, emTransfer;
-
-  struct DialInfo { //preface to a multi-generator option beyond NEUT
+  struct DialInfo {
     std::string prettyname;
+    size_t pid;
+    std::vector<std::unique_ptr<TH3D>> alt_xs;
   };
   std::vector<DialInfo> dial_infos;
 
   // configurable verbosity as an example of some arbitrary systprovider
   // configuration
   int verbosity_level;
-
-  float Enu_true, Q2, q0, m_nucleon, W_nuc_rest;
-
-  const double m_proton = 0.93827203;   // Proton mass in GeV - set identically to nuisance
-
 };
